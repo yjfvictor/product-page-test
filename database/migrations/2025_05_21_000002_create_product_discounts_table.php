3b282d0964172234d327f9240030b6d88d27911b
Victor Yeh <yjf_victor@hotmail.com>
2025-05-21 10:45:34 +1000

    Create databases using Laravel migrations

---
+++ database/migrations/2025_05_21_000002_create_product_discounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_discounts', function (Blueprint $table) {
+            $table->id();
+	    $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+	    $table->enum('type', ['percent', 'amount']);
+            $table->integer('discount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_discounts');
+    }
+}
